9aae7d404b7f4c209c64b5ef4e99dfac1f8ff42e
Glitch (wd-server5) <none>
2024-03-31 21:59:54 +0000

    🍭🛰 Checkpoint ./server.js:28824481/1601

--- server.js
+++ server.js
@@ -5443,17 +5443,38 @@ if (ArenaClosed !== true) {
                     let o = new Entity(room.random());
                     o.color = 17;
                     o.define(Class.bot);
-                       let arrayOfClasses = [Class.spike, Class.stream, Class.overseer, Class.overlord, Class.weirdspike, Class.fighter,
-                                        Class.machinegunner, Class.penta, Class.octo, Class.baby_fac, Class.jr_closer, Class.tri, Class.flank, Class.mach2,
-                                        Class.hybrid, Class.anni, Class.destroy, Class.booster, Class.mega_anni, Class.ninja, Class.RAIL, Class.grower ]                  
+                       let arrayOfClasses = [Class.spike, Class.overlord, Class.fighter,
+                                        Class.machinegunner, Class.penta, Class.octo, Class.jr_closer,  Class.mach2,
+                                        Class.hybrid, Class.anni, Class.destroy, Class.booster, ]                  
                       let newClass = arrayOfClasses[Math.floor(Math.random() * arrayOfClasses.length)];
-                    o.define(newClass);
+                 o.define(newClass)
                 
                     o.name += ran.chooseBotName();
                     o.refreshBodyAttributes();
                                   
                     o.color = 12;
                     o.team = -3;
+          
+                    bots.push(o)
+                    
+                }
+          
+           if (bots.length < c.BOTS) {
+                    let o = new Entity(room.random());
+                    o.color = 17;
+                    o.define(Class.bot);
+                       let arrayOfClasses = [  Class.overseer, Class.overlord,  Class.fighter, 
+                                        Class.machinegunner, Class.penta, Class.octo, Class.mach2,
+                                        Class.hybrid, Class.anni, Class.destroy, Class.booster, ]                  
+                      let newClass = arrayOfClasses[Math.floor(Math.random() * arrayOfClasses.length)];
+              o.define(newClass)
+                
+                    o.name += ran.chooseBotName();
+                    o.refreshBodyAttributes();
+                                  
+                    o.color = 10;
+                    o.team = -1;
+                  
           
                     bots.push(o)
                     
